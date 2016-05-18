67f2ec30ac89c2139b09c4431d2499034119a9b4
Eugene Glotov <kivagant@gmail.com>
2016-05-18 18:56:16 +0300

    LK-4147 Basic implementation for AclMiddleware with roles and action (all users still looks like guests)

--- src/Staticus/Acl/AclMiddleware.php
+++ src/Staticus/Acl/AclMiddleware.php
@@ -6,6 +6,8 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Staticus\Diactoros\FileContentResponse\ResourceDoResponse;
 use Staticus\Exceptions\WrongRequestException;
+use Zend\Diactoros\Response\EmptyResponse;
+use Zend\Permissions\Acl\Resource\ResourceInterface;
 use Zend\Stratigility\MiddlewareInterface;
 
 class AclMiddleware implements MiddlewareInterface
@@ -25,13 +27,26 @@ class AclMiddleware implements MiddlewareInterface
         callable $next = null
     )
     {
+        /** @var $response ResourceDoResponse */
         $this->checkResponseOrDie($response);
 
-//        return new \Zend\Diactoros\Response\JsonResponse(
-//            $this->service->acl()->isAllowed(
-//                Roles::USER,
-//                \Staticus\Resources\Jpg\ResourceDO::class,
-//                Actions::ACTION_DELETE));
+        $method = $request->getMethod();
+        $action = $this->getAction($method);
+        $resourceDO = $response->getContent();
+        $AclResourceCommon = get_class($resourceDO);
+        $AclResourceUnique = $resourceDO instanceof ResourceInterface
+            ? $resourceDO->getResourceId()
+            : null;
+        $role = Roles::GUEST;// @todo get roles from authenticated user
+        if (!$this->isAllowed($role, $AclResourceCommon, $action)) {
+
+            return new EmptyResponse(403);
+        }
+        if (!$this->isAllowed($role, $AclResourceUnique, $action)) {
+
+            return new EmptyResponse(403);
+        }
+
         return $next($request, $response);
     }
 
@@ -56,5 +71,42 @@ class AclMiddleware implements MiddlewareInterface
     protected function isSupportedResponse(ResponseInterface $response)
     {
         return $response instanceof ResourceDoResponse;
+    }
+
+    /**
+     * @param $method
+     * @return string
+     */
+    protected function getAction($method)
+    {
+        switch ($method) {
+            case 'GET':
+                $action = Actions::ACTION_READ;
+                break;
+            case 'POST':
+                $action = Actions::ACTION_WRITE;
+                break;
+            case 'DELETE':
+                $action = Actions::ACTION_DELETE;
+                break;
+            default:
+                throw new WrongRequestException(
+                    'Unknown access control action',
+                    __LINE__);
+        }
+
+        return $action;
+    }
+
+    protected function isAllowed($role, $AclResourceName, $action)
+    {
+        return (
+            !$this->service->acl()->hasRole($role)
+            || !$this->service->acl()->hasResource($AclResourceName)
+            || $this->service->acl()->isAllowed(
+                $role,
+                $AclResourceName,
+                $action)
+        );
     }
 }
