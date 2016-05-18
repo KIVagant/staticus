d6b5e120c461e2db7a7f217100abde550d3a83f6
Eugene Glotov <kivagant@gmail.com>
2016-05-18 10:52:23 +0300

    LK-4147 Config now implements ConfigInterface

--- src/Staticus/Config/Config.php
+++ src/Staticus/Config/Config.php
@@ -1,26 +1,58 @@
 <?php
 namespace Staticus\Config;
 
-class Config
+class Config implements ConfigInterface
 {
     protected $config = [];
     public function __construct($config)
     {
         $this->config = $config;
     }
-    public function get($route)
+
+    public function all()
     {
+
+        return $this->config;
+    }
+
+    public function get($route, $default = null)
+    {
+        $config = $this->config;
         $routes = explode('.', $route);
         $endPath = '';
-        $config = $this->config;
         foreach ($routes as $item) {
             $endPath .= $item . '.';
-            if (!isset($config[$item])) {
-                throw new \RuntimeException('Unknown config route: ' . $endPath, __LINE__);
+            if (!array_key_exists($item, $config)) {
+                if (null !== $default) {
+
+                    return $default;
+                }
+                throw new \RuntimeException('Unknown config route and no default values: ' . $endPath, __LINE__);
             }
             $config = $config[$item];
         }
 
         return $config;
+    }
+
+    /**
+     * @param $route
+     * @return bool
+     */
+    public function has($route)
+    {
+        $config = $this->config;
+        $routes = explode('.', $route);
+        $endPath = '';
+        foreach ($routes as $item) {
+            $endPath .= $item . '.';
+            if (!array_key_exists($item, $config)) {
+
+                return false;
+            }
+            $config = $config[$item];
+        }
+
+        return true;
     }
 }
