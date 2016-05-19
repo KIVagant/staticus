497cc02a7e8cea07cbd23754ca296a3be19e5c5f
Eugene Glotov <kivagant@gmail.com>
2016-05-19 13:04:38 +0300

    LK-4147 environments variable 'SESSION_NAMESPACE' renamed to 'SESSION_COOKIE_NAME'

--- config/autoload/auth.global.php
+++ config/autoload/auth.global.php
@@ -12,16 +12,16 @@ return [
     'auth' => [
         'session' => [
             'redis' => [
-                    'host' => env('REDIS_HOST', true),
-                    'port' => env('REDIS_PORT', true),
-                    'password' => env('REDIS_PASS', true),
+                    'host' => env('REDIS_HOST', '127.0.0.1'),
+                    'port' => env('REDIS_PORT', 6379),
+                    'password' => env('REDIS_PASS'),
                 ],
             'options' => [
                     'use_cookies' => env('SESSION_COOKIE_ENABLED', true),
                     'cookie_secure' => env('SESSION_COOKIE_SECURE', true),
                     'cookie_lifetime' => env('SESSION_GC_MAXLIFETIME', 31536000), // 1 year
                     'gc_maxlifetime' => env('SESSION_GC_MAXLIFETIME', 1728000), // 20 days
-                    'name' => env('SESSION_NAMESPACE', true),
+                    'name' => env('SESSION_COOKIE_NAME'),
                 ],
             ],
         'basic' => [
