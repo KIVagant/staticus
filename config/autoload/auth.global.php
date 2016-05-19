ba07a2cb6a597feae04e2f89b00702cdf1a96302
Eugene Glotov <kivagant@gmail.com>
2016-05-19 10:56:50 +0300

    LK-4147 SessionManager, Factory and basic configuration

--- config/autoload/auth.global.php
+++ config/autoload/auth.global.php
@@ -1,8 +1,25 @@
 <?php
 
 return [
+    'dependencies' => [
+        'factories' => [
+            Zend\Session\SessionManager::class => Staticus\Auth\Factories\SessionManagerFactory::class,
+        ],
+    ],
     'auth' => [
         'session' => [
+            'redis' => [
+                    'host' => env('REDIS_HOST', true),
+                    'port' => env('REDIS_PORT', true),
+                    'password' => env('REDIS_PASS', true),
+                ],
+            'options' => [
+                    'use_cookies' => env('SESSION_COOKIE_ENABLED', true),
+                    'cookie_secure' => env('SESSION_COOKIE_SECURE', true),
+                    'cookie_lifetime' => env('SESSION_GC_MAXLIFETIME', 31536000), // 1 year
+                    'gc_maxlifetime' => env('SESSION_GC_MAXLIFETIME', 1728000), // 20 days
+                    'name' => env('SESSION_NAMESPACE', true),
+                ],
             ],
         'basic' => [
             'users' => [
