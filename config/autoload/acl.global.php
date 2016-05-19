daf24429d3e61b1d329f122078089eeec24d6a1d
Eugene Glotov <kivagant@gmail.com>
2016-05-19 17:22:15 +0300

    LK-4147 ACL rules for admin, guests in common namespaces, users in their namespace

--- config/autoload/acl.global.php
+++ config/autoload/acl.global.php
@@ -2,6 +2,8 @@
 
 use Staticus\Acl\AclService;
 use Staticus\Acl\Roles;
+use Staticus\Auth\UserInterface;
+use Staticus\Resources\ResourceDOInterface;
 
 return [
     'dependencies' => [
@@ -32,32 +34,101 @@ return [
             ],
         ],
         AclService::RESOURCES => [
-            \Staticus\Resources\File\ResourceDO::class => [
+
+            // ---------------------------------------------------------------------------------------------------------
+            // ADMIN RULES
+            Staticus\Resources\File\ResourceDO::class => [
                 AclService::PRIVILEGES => [
-                    Roles::GUEST => [
-                        \Staticus\Acl\Actions::ACTION_READ,
-                    ],
                     Roles::ADMIN => [
-                        \Staticus\Acl\Actions::ACTION_WRITE,
-                        \Staticus\Acl\Actions::ACTION_SEARCH,
-                        \Staticus\Acl\Actions::ACTION_DELETE,
+                        Staticus\Acl\Actions::ACTION_READ,
+                        Staticus\Acl\Actions::ACTION_WRITE,
+                        Staticus\Acl\Actions::ACTION_SEARCH,
+                        Staticus\Acl\Actions::ACTION_DELETE,
                     ]
                 ],
             ],
-            \Staticus\Resources\Mpeg\ResourceDO::class => [
-                AclService::INHERIT => \Staticus\Resources\File\ResourceDO::class,
+            Staticus\Resources\Mpeg\ResourceDO::class => [
+                AclService::INHERIT => Staticus\Resources\File\ResourceDO::class,
             ],
-            \Staticus\Resources\Image\ResourceImageDO::class => [
-                AclService::INHERIT => \Staticus\Resources\File\ResourceDO::class,
+            Staticus\Resources\Image\ResourceImageDO::class => [
+                AclService::INHERIT => Staticus\Resources\File\ResourceDO::class,
             ],
-            \Staticus\Resources\Jpg\ResourceDO::class => [
-                AclService::INHERIT => \Staticus\Resources\Image\ResourceImageDO::class,
+            Staticus\Resources\Jpg\ResourceDO::class => [
+                AclService::INHERIT => Staticus\Resources\Image\ResourceImageDO::class,
             ],
-            \Staticus\Resources\Png\ResourceDO::class => [
-                AclService::INHERIT => \Staticus\Resources\Image\ResourceImageDO::class,
+            Staticus\Resources\Png\ResourceDO::class => [
+                AclService::INHERIT => Staticus\Resources\Image\ResourceImageDO::class,
             ],
-            \Staticus\Resources\Gif\ResourceDO::class => [
-                AclService::INHERIT => \Staticus\Resources\Image\ResourceImageDO::class,
+            Staticus\Resources\Gif\ResourceDO::class => [
+                AclService::INHERIT => Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+
+            // ---------------------------------------------------------------------------------------------------------
+            // Rules for the common resources namespace
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\File\ResourceDO::class => [
+                AclService::PRIVILEGES => [
+                    Roles::GUEST => [
+                        Staticus\Acl\Actions::ACTION_READ,
+                    ],
+                ],
+            ],
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\Mpeg\ResourceDO::class => [
+                AclService::INHERIT => ResourceDOInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\File\ResourceDO::class,
+            ],
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\Image\ResourceImageDO::class => [
+                AclService::INHERIT => ResourceDOInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\File\ResourceDO::class,
+            ],
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\Jpg\ResourceDO::class => [
+                AclService::INHERIT => ResourceDOInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\Png\ResourceDO::class => [
+                AclService::INHERIT => ResourceDOInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+            ResourceDOInterface::NAMESPACES_WILDCARD . Staticus\Resources\Gif\ResourceDO::class => [
+                AclService::INHERIT => ResourceDOInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+
+            // ---------------------------------------------------------------------------------------------------------
+            // Rules inside home namespace for user
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\File\ResourceDO::class => [
+                AclService::PRIVILEGES => [
+                    // Guests can see users files
+                    Roles::GUEST => [
+                        Staticus\Acl\Actions::ACTION_READ,
+                    ],
+
+                    // Users can modify users files
+                    Roles::USER => [
+                        Staticus\Acl\Actions::ACTION_WRITE,
+                        Staticus\Acl\Actions::ACTION_SEARCH,
+                        Staticus\Acl\Actions::ACTION_DELETE,
+                    ],
+                ],
+            ],
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\Mpeg\ResourceDO::class => [
+                AclService::INHERIT => UserInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\File\ResourceDO::class,
+            ],
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\Image\ResourceImageDO::class => [
+                AclService::INHERIT => UserInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\File\ResourceDO::class,
+            ],
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\Jpg\ResourceDO::class => [
+                AclService::INHERIT => UserInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\Png\ResourceDO::class => [
+                AclService::INHERIT => UserInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
+            ],
+            UserInterface::NAMESPACES_WILDCARD . Staticus\Resources\Gif\ResourceDO::class => [
+                AclService::INHERIT => UserInterface::NAMESPACES_WILDCARD
+                    . Staticus\Resources\Image\ResourceImageDO::class,
             ],
         ],
     ],
