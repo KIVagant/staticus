d116e0f653eb935544f2506967dd34ddb2e0f3d3
Eugene Glotov <kivagant@gmail.com>
2016-05-27 17:57:10 +0300

    LK-3698 composer require "newage/audio-manager": "^1.0"

--- src/App/Actions/Voice/VoiceAdapterFactory.php
+++ src/App/Actions/Voice/VoiceAdapterFactory.php
@@ -4,12 +4,23 @@ namespace App\Actions\Voice;
 
 use AudioManager\Adapter\Google;
 use AudioManager\Adapter\Ivona;
+use Staticus\Config\ConfigInterface;
+use Staticus\Config\Config;
 use Staticus\Exceptions\ErrorException;
 
 class VoiceAdapterFactory
 {
     const VOICE_PROVIDER_GOOGLE = 'google';
     const VOICE_PROVIDER_IVONA = 'ivona';
+    /**
+     * @var ConfigInterface|Config
+     */
+    protected $config;
+
+    public function __construct(ConfigInterface $config)
+    {
+        $this->config = $config;
+    }
 
     /**
      * @return \AudioManager\Adapter\AdapterInterface
@@ -17,21 +28,19 @@ class VoiceAdapterFactory
      */
     public function __invoke()
     {
-        $adapterName = strtolower(env('VOICE_DEFAULT_PROVIDER', self::VOICE_PROVIDER_GOOGLE));
+        $adapterName = strtolower($this->config->get('voice.provider', self::VOICE_PROVIDER_GOOGLE));
         switch ($adapterName) {
             case self::VOICE_PROVIDER_GOOGLE:
-                $options = new Google\Options();
-                $options->setLanguage('en');
-                $options->setEncoding('UTF-8');
-                $adapter = new Google($options);
+                $adapter = new Google();
+                $adapter->getOptions()->setLanguage('en');
+                $adapter->getOptions()->setEncoding('UTF-8');
                 break;
             case self::VOICE_PROVIDER_IVONA:
-                $secretKey = env('VOICE_IVONA_SECRET_KEY');
-                $accessKey = env('VOICE_IVONA_ACCESS_KEY');
-                $authenticate = new Ivona\Authenticate($secretKey, $accessKey);
-                $options = new Ivona\Options($authenticate);
-                $adapter = new Ivona($options);
-                $adapter->setOptions($options);
+                $secretKey = $this->config->get('voice.' . $adapterName . '.secret_key', '');
+                $accessKey = $this->config->get('voice.' . $adapterName . '.access_key', '');
+                $adapter = new Ivona();
+                $adapter->getOptions()->setSecretKey($secretKey);
+                $adapter->getOptions()->setAccessKey($accessKey);
                 break;
             default:
                 throw new ErrorException('Not implemented functionality for voice provider: ' . $adapterName, __LINE__);
