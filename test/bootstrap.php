3e99aa10687bd5c80108a3b7f6f7766cda96d558
Eugene Glotov <KIVagant@gmail.com>
2016-06-03 20:56:12 +0300

    bootstrap autoloading probably fix

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php'; // For the vendor sub-folders tests
-require 'config/bootstrap.php';
+require __DIR__ . '/../vendor/autoload.php'; // For the vendor sub-folders tests
+require __DIR__ . '/../config/bootstrap.php';
 $dotenv = new Dotenv\Dotenv('./');
 $dotenv->load();
